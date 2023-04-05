9961b1bfcbc020f6007f62505bd20838ceb6930b
ramatc <rtanquiascornejo@gmail.com>
2023-04-04 22:08:45 -0300

    Clase del 04-04

---
+++ Clase_04-04/Tipos_Primitivos/Tipos_Primitivos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Clase4_ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort corto;
+            float flotante;
+            char caracter, caracterInvertido;
+            string cadena, invertida = "";
+            bool booleano;
+
+            Console.Write("Ingrese un valor numerico corto: ");
+            corto = ushort.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un flotante: ");
+            flotante = float.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese un caracter: ");
+            caracter = char.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese una cadena de caracteres: ");
+            cadena = Console.ReadLine();
+
+            Console.Write("Ingrese un valor booleano: ");
+            booleano = bool.Parse(Console.ReadLine());
+
+            if (char.IsLower(caracter)) caracterInvertido = Char.ToUpper(caracter);
+            else caracterInvertido = Char.ToLower(caracter);
+
+            for (int i = cadena.Length - 1; i >= 0; i--)
+            {
+                invertida += cadena[i];
+            }
+
+            Console.WriteLine($"1) {corto + 1}");
+            Console.WriteLine($"2) {corto + flotante}");
+            Console.WriteLine($"3) {(char)(caracter + 1)}");
+            Console.WriteLine($"4) {(caracterInvertido)}");
+            Console.WriteLine($"5) {invertida}");
+            Console.WriteLine($"6) {!booleano}");
+
+            Console.WriteLine("\nPresione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase_04-04/Triangulo/Triangulo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Triangulos
+{
+    class Program
+    {
+        static void Main()
+        {
+            float ladoA, ladoB, ladoC;
+
+            Console.Write("Ingresé el lado A del triangulo: ");
+            while (!float.TryParse(Console.ReadLine(), out ladoA) || ladoA < 0)
+            {
+                Console.Write("Ingresé una longitud valida: ");
+            }
+
+            Console.Write("Ingresé el lado B del triangulo: ");
+            while (!float.TryParse(Console.ReadLine(), out ladoB) || ladoB < 0)
+            {
+                Console.Write("Ingresé una longitud valida: ");
+            }
+
+            Console.Write("Ingresé el jlado C del triangulo: ");
+            while (!float.TryParse(Console.ReadLine(), out ladoC) || ladoC < 0)
+            {
+                Console.Write("Ingresé una longitud valida: ");
+            }
+
+            if (ladoA == ladoB && ladoB == ladoC)
+            {
+                Console.WriteLine("El triángulo es equilátero");
+            }
+            else if (ladoA != ladoB && ladoB != ladoC && ladoC != ladoA)
+            {
+                Console.WriteLine("El triángulo es escaleno");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo es isósceles");
+            }
+
+            Console.WriteLine("\nPresione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
