9b50b7754d434fd35e8d349b96decaa8dd34ac0a
ramatc <rtanquiascornejo@gmail.com>
2023-05-08 20:12:31 -0300

    Modelo de parcial CReserva

---
+++ Modelos_Parcial_1/CReserva/CReserva/CEjecutora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Sala_De_Ensayo
+{
+    public class CEjecutora
+    {
+        public static void Main()
+        {
+            float total = 0;
+            ulong num_reserva;
+            string inicio, banda;
+            ushort duracion_reserva;
+            CReserva res1, maymon = new CReserva("", 0, "");
+
+            Console.WriteLine("Ingrese el costo por hora de la reserva: ");
+            CReserva.Cost = (float.Parse(Console.ReadLine()));
+            Console.WriteLine("Ingrese el descuento: ");
+            CReserva.Desc = (float.Parse(Console.ReadLine()));
+
+            Console.Write("\nIngrese número de reserva (0 para terminar): ");
+            while (ulong.TryParse(Console.ReadLine(), out num_reserva) != true)
+            {
+                Console.Write("Ingresé un número de reserva valido: ");
+            }
+            if (num_reserva == 0)
+            {
+                Console.WriteLine("No se ingresaron reservas validas");
+            }
+            else
+            {
+                while (num_reserva != 0)
+                {
+                    Console.Write("Ingresé el inicio de la reserva: ");
+                    inicio = Console.ReadLine();
+
+                    Console.Write("Ingresé la duración de la reserva: ");
+                    duracion_reserva = ushort.Parse(Console.ReadLine());
+
+                    Console.Write("Ingresé el nombre de la banda: ");
+                    banda = Console.ReadLine();
+
+                    res1 = new CReserva(inicio, duracion_reserva, banda);
+                    res1.Numres = (num_reserva);
+
+                    Console.WriteLine("Costo: " + res1.DarMontoTotal() + "$");
+                    total += res1.DarMontoTotal(450);
+
+                    if (CReserva.MayorMonto(res1, maymon) == res1) maymon = res1;
+
+                    Console.Write("\nIngrese número de reserva (0 para terminar): ");
+                    num_reserva = ulong.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("\n\n" + maymon.DarDatos() + "\n\nEl total recaudado es de " + total + " Dolares ( a 450 pesos) o " + total * 450 + " pesos\n\n");
+            }
+        }
+    }
+}
---
+++ Modelos_Parcial_1/CReserva/CReserva/CReserva.cs
@@ -0,0 +1,93 @@
+﻿namespace Sala_De_Ensayo
+{
+    public class CReserva
+    {
+        private ulong numero_reserva;
+        private string inicio_reserva;
+        private ushort duracion;
+        private static float costo_por_hora;
+        private static float descuento;
+        private string banda;
+
+        // Ejercicio 5
+        public CReserva(string inicio, ushort duracion, string Band)
+        {
+            this.inicio_reserva = inicio;
+            this.duracion = duracion;
+            this.banda = Band;
+        }
+
+        // Ejercicios 2 y 6
+        public ulong Numres
+        {
+            get { return this.numero_reserva; }
+            set { this.numero_reserva = value; }
+        }
+
+        // Ejercicio 3
+        public static float Cost
+        {
+            set { CReserva.costo_por_hora = value; }
+        }
+
+        // Ejercicio 4
+        public static float Desc
+        {
+            set { CReserva.descuento = value; }
+        }
+
+        // Ejercicio 10
+        public string Band
+        {
+            get { return this.banda; }
+            set { this.banda = value; }
+        }
+
+        // Ejercicio 7
+        public float DarMontoTotal()
+        {
+            if (this.duracion >= 6)
+            {
+                return (this.duracion * CReserva.costo_por_hora) * (1 - (CReserva.descuento / 100));
+            }
+            return (float)this.duracion * CReserva.costo_por_hora;
+        }
+
+        // Ejercicio 8
+        public float DarMontoTotal(float cotizacion)
+        {
+            if (this.duracion >= 6)
+            {
+                return ((this.duracion * CReserva.costo_por_hora) * (1 - (CReserva.descuento / 100))) / cotizacion;
+            }
+            return ((float)this.duracion * CReserva.costo_por_hora) / cotizacion;
+        }
+
+        // Ejercicio 9
+        public string DarDatos()
+        {
+            string datos = "Numero de reserva: " + this.numero_reserva.ToString();
+            datos += "\nInicio de reserva: " + this.inicio_reserva;
+            datos += "\nDuracion: " + this.duracion.ToString();
+            datos += "\nCosto por hora: " + CReserva.costo_por_hora.ToString() + "$";
+            datos += "\nDescuento: " + CReserva.descuento.ToString() + "%";
+            datos += "\nBanda: " + this.banda;
+            datos += "\nTotal: " + this.DarMontoTotal().ToString() + "$";
+            return (datos);
+        }
+
+        // Ejercicio 11
+        public static CReserva MayorMonto(CReserva reserva1, CReserva reserva2)
+        {
+            if (reserva1.DarMontoTotal() > reserva2.DarMontoTotal())
+            {
+                return reserva1;
+            }
+            else
+            {
+                return reserva2;
+            }
+
+        }
+    }
+}
