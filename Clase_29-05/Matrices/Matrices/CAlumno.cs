620542db47b164d82047f77c7b2e2d3199569cf5
ramatc <rtanquiascornejo@gmail.com>
2023-06-05 19:00:41 -0300

    Ejercicio matrices

---
+++ Clase_29-05/Matrices/Matrices/CAlumno.cs
@@ -0,0 +1,94 @@
+﻿namespace Facultad
+{
+    public class CAlumno
+    {
+        private static float Cuota;
+
+        private ulong legajo;
+        private string apellidos;
+        private string nombres;
+        private float beca;
+
+        public static void SetCuota(float cuo)
+        {
+            CAlumno.Cuota = cuo;
+        }
+        public static float GetCuota(float cuo)
+        {
+            return CAlumno.Cuota;
+        }
+
+        public CAlumno()
+        {
+            this.legajo = 99999999UL;
+            this.apellidos = "N";
+            this.nombres = "N";
+            this.beca = 0.0F;
+        }
+        public CAlumno(ulong legajo, string apellidos, string nombres, float beca)
+        {
+            this.legajo = legajo;
+            this.apellidos = apellidos;
+            this.nombres = nombres;
+            this.beca = beca;
+        }
+        public CAlumno(ulong leg)
+        {
+            this.legajo = leg;
+        }
+
+        //Setters
+        public void SetLegajo(ulong leg)
+        {
+            this.legajo = leg;
+        }
+        public void SetApellidos(string ape)
+        {
+            this.apellidos = ape;
+        }
+        public void SetNombres(string nom)
+        {
+            this.nombres = nom;
+        }
+        public void SetBeca(float beca)
+        {
+            this.beca = beca;
+        }
+
+        //Getters
+        public ulong GetLegajo()
+        {
+            return this.legajo;
+        }
+        public string GetApellidos()
+        {
+            return this.apellidos;
+        }
+        public string GetNombres()
+        {
+            return this.nombres;
+        }
+        public float GetBeca() 
+        { 
+            return this.beca;
+        }
+
+        //Funcionales
+        public float DarCuotaAlumno()
+        {
+            float cuo = CAlumno.Cuota;
+            float monto = cuo * (1 - (this.beca / 100.0F));
+            return monto;
+        }
+        
+        public string DarDatos()
+        {
+            string datos = "Alumno de legajo: " + this.legajo.ToString();
+            datos += " -Nombre completo: " + this.apellidos + ", " + this.nombres;
+            datos += " -Cuota general: $" + CAlumno.Cuota.ToString();
+            datos += " -Beca: " + this.beca.ToString() + "%";
+            datos += " -Monto a pagar final: $" + this.DarCuotaAlumno().ToString();
+            return datos;
+        }
+    }
+}
---
+++ Clase_29-05/Matrices/Matrices/CEjecutora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Facultad
+{
+    public class CEJecutora
+    {
+        public static void Main(string[] args)
+        {
+            CAlumno auxAl;
+            CAlumno[] vecAl;
+            vecAl = new CAlumno[8];
+            auxAl = new CAlumno(10731UL, "Pérez", "Rocio", 0);
+            vecAl[0] = auxAl;
+            vecAl[2] = new CAlumno(10755UL, "Ruiz", "Pablo", 0);
+            vecAl[3] = new CAlumno();
+            vecAl[3].SetLegajo(10722UL);
+            CAlumno alumno3 = (CAlumno)vecAl.GetValue(3);
+            alumno3.SetBeca(50.0f);
+            vecAl[1] = vecAl[3];
+
+            for(int cont = 0; cont < vecAl.GetLength(0); cont++)
+            {
+                if (vecAl[cont] != null)
+                {
+                    Console.WriteLine("N{0}: {1}", cont, vecAl[cont].DarDatos());
+                }
+            }
+
+            Console.WriteLine();
+            foreach (CAlumno aux in vecAl)
+            {
+                Console.WriteLine("{0}", aux.DarDatos());
+            }
+        }
+    }
+}
